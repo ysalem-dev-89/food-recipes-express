42ed94a5bb16c9ea28fac43e659144ef823ecc4d
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-09-26 23:44:56 +0300

    fixed

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 app.get('/api/dishes', (req, res) => {
   fs.readFile(path.join(__dirname, './data.json'), (err, result) => {
