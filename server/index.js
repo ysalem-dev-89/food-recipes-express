e717bb581361841361f850d1cd13b4b30c51c840
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-09-26 23:48:06 +0300

    fiexed

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
